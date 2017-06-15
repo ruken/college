2f5054bf091a7d1d73b2cdd1f5737dc8986565fb
ruken <oralruken@gmail.com>
2017-06-14 21:49:37 -0700

    added @Autowire in StudentJpaRepository

--- src/main/java/org/ruken/college/controller/CollegeController.java
+++ src/main/java/org/ruken/college/controller/CollegeController.java
@@ -6,14 +6,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.ruken.json.Student;
+
 
 @RestController
 public class CollegeController {
 
-	//@RequestMapping(method = RequestMethod.POST)
-	//ResponseEntity<?> add(@PathVariable String userId, @RequestBody Bookmark input) {
-
-	//}
+	
+	
+	@Autowire 
+	StudentJpaRepository studentRepo;
+	
+	@RequestMapping(value="/addStudent", method = RequestMethod.POST)
+	ResponseEntity<Student> addStudent(@RequestBody Student input) {
+      System.out.println("adding Student: " + input);
+      
+      
+      
+      return null;
+	}
+	
 	
     @RequestMapping("/")
     public String index() {
